badd356b2793be04d54c581fe9bc94f2e2ca9eb5
Honoré Vasconcelos <honore.vasconcelos@gmail.com>
2018-06-09 12:43:55 +0100

    Fix solution and add proposed solution

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,6 +7,7 @@ use bencher::Bencher;
 use projecteuler::problem1;
 use projecteuler::problem2;
 use projecteuler::problem3;
+use projecteuler::problem4;
 
 fn problem1_iterative(b: &mut Bencher) {
     b.iter(||{problem1::iterative_sum_of_divisible_by_3_and_5(500)});
@@ -25,12 +26,22 @@ fn problem3_sol1(b: &mut Bencher) {
     b.iter(||{problem3::largest_prime_factor(600851475143)});
 }
 
+fn problem4_sol1(b: &mut Bencher) {
+    b.iter(||{problem4::get_highest_palindrome_generated_by_two_number_of_n_digits(3)});
+}
+
+fn problem4_optimal(b: &mut Bencher) {
+    b.iter(||{problem4::get_highest_palindrome_optimally_generated_by_two_number_of_3_digits()});
+}
+
 benchmark_group!(
     bench, 
     problem1_iterative, 
     problem1_optimal,
     problem2_sol1,
     problem3_sol1,
+    problem4_sol1,
+    problem4_optimal,
     );
 
 benchmark_main!(bench);
--- src/problem4.rs
+++ src/problem4.rs
@@ -1,24 +1,96 @@
 //! #Problem 4
 //! A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 //! Find the largest palindrome made from the product of two 3-digit numbers.
-struct HalfPalindrome {
-    half:u32,
-    is_odd:bool
+
+
+/// Compute highest palindrome computed from number with n digits
+pub fn get_highest_palindrome_generated_by_two_number_of_n_digits(digits:u32) -> Option<u32> {
+    match get_numbers_to_generate_max_palindrome_with_n_digits(digits) {
+        Some((x,y)) => Some(x*y),
+        None => None
+    }
 }
 
+/// Optimal solution given by projec teuler 
+pub fn get_highest_palindrome_optimally_generated_by_two_number_of_3_digits() -> Option<u32> {
+    let mut largest_palindrome = 0;
+    let mut a = 999;
+    while a >= 100 {
+        let mut b = 999;
+        while b >= a {
+            if a*b <= largest_palindrome {
+                break; //Since a*b is always going to be too small
+            }
+            if is_palindrome(a*b) {
+                largest_palindrome = a*b;
+            }
+            b = b-1;
+        }
+        a = a-1;
+    }
+    Some(largest_palindrome)
+}
+
+#[derive(Debug)]
+struct Palindrome {
+    //full value of palindrome
+    p:u32,
+    //size of the palindrome to avoid calculating it every time
+    size:u32,
+    //half part to enable simple subtraction to go dow in palindromes
+    half:u32,
+    //minimal value of half do detect when the size of the palindrome has changed
+    min_half:u32,
+    //palindrome is odd or even to know if last digit of half should be repeated
+    is_odd:bool,
+}
+
+impl Palindrome {
+    fn new(p:u32) -> Palindrome {
+        if !is_palindrome(p) {
+            panic!("Invalid palindrome {}", p);
+        }
+        let size = number_of_digits(p);
+        let is_odd =  size % 2 != 0;
+        let half = if is_odd {
+            size /2 +1
+        }else{
+            size /2 
+        };
+        let min_half = power_of_ten(half)/10;
+        Palindrome{ p, size, half: p / power_of_ten(size-half), min_half, is_odd}
+    }
+    
+    //compute previous palindrome 
+    fn mutate_to_prev(&mut self) {
+        self.half-=1;
+        if self.half < self.min_half {
+            self.is_odd = !self.is_odd;
+            self.size-=1;
+            if !self.is_odd {
+                self.min_half = self.min_half / 10;
+            }else{
+                self.half = self.half * 10 + 9;
+            }
+        }
+        //TODO this odd value is incorrect. was valid for previous value of half
+        self.p = half_to_full_palindrome(self.half, self.is_odd);
+    }
+}
+
+/// Get (x,y), where x and y have the size of n digits that can produce the highest palindrome 
 pub fn get_numbers_to_generate_max_palindrome_with_n_digits(digits:u32)  -> Option<(u32,u32)> {
     let max_oper = power_of_ten(digits) - 1;
-    let max = first_palindrome_from(max_oper.pow(2));
-    let mut p = first_half_of_palindrome(max);
+    let max = first_palindrome_below(max_oper.pow(2));
+    let mut p = Palindrome::new(max);
     while p.half > 0 {
-        let n = half_to_full_palindrome(&p);
+        let n = p.p;
         for i in (max_oper/2)..=max_oper {
             if n % i == 0 && i < max_oper && number_of_digits(n / i) == digits {
                 return Some((i, n / i));
             }
         }
-        p.half-=1;
-        //TODO set is_odd correctly
+        p.mutate_to_prev();
     }
     None
 }
@@ -27,6 +99,7 @@ fn power_of_ten(digits:u32) -> u32 {
     (10 as u32).pow(digits)
 }
 
+/// Compute the number of digits 
 fn number_of_digits(v:u32) -> u32 {
     let mut size = 0;
     let mut v = v;
@@ -37,21 +110,12 @@ fn number_of_digits(v:u32) -> u32 {
     size
 }
 
-fn first_half_of_palindrome(v:u32) -> HalfPalindrome {
-    let size = number_of_digits(v);
-    let is_odd =  size % 2 != 0;
-    let half = if is_odd {
-        size /2 +1
-    }else{
-        size /2 
-    };
-    HalfPalindrome{ half: v / power_of_ten(half), is_odd} 
-}
-
-fn half_to_full_palindrome(p:&HalfPalindrome) -> u32{
-    let mut res = p.half;
-    let mut v = p.half;
-    if p.is_odd {
+/// Make a palindrome out of half a palindrome. if is_odd is true, last digit is to be 
+/// placed twice in the center
+fn half_to_full_palindrome(half:u32, is_odd:bool) -> u32{
+    let mut res = half;
+    let mut v = half;
+    if is_odd {
         v = v / 10;
     }
     
@@ -62,7 +126,8 @@ fn half_to_full_palindrome(p:&HalfPalindrome) -> u32{
     res
 }
 
-fn first_palindrome_from(max:u32) -> u32 {
+// Find first palindrome below max number
+fn first_palindrome_below(max:u32) -> u32 {
     let mut i = max;
     if i > 10 {
         //reduce the number of iteration required to get first palindrome
@@ -80,23 +145,16 @@ fn first_palindrome_from(max:u32) -> u32 {
     i
 }
 
+
+// verify if given n is a palindrome
 fn is_palindrome(n:u32) -> bool {
-    let mut v = vec![];
-    let mut r = n;
-    while r > 0 {
-        v.push(r % 10);
-        r = r / 10;
+    let mut n2 = n;
+    let mut reverse = 0;
+    while n2 > 0 {
+        reverse = reverse * 10 + n2 % 10;
+        n2 = n2/10;
     }
-    let mut a = 0;
-    let mut b = v.len()-1;
-    while a<=b {
-        if v[a] != v[b] {
-            return false;
-        }
-        a+=1;
-        b-=1;
-    }
-    true
+    reverse == n
 }
 
 
@@ -106,13 +164,44 @@ mod tests {
 
     #[test]
     fn first_palindrome_is_correct() {
-        assert_eq!(997799, first_palindrome_from(999*999));
+        assert_eq!(997799, first_palindrome_below(999*999));
+    }
+
+    #[test]
+    fn give_correct_solutions() {
+        assert_eq!(9009, get_highest_palindrome_generated_by_two_number_of_n_digits(2).unwrap());
+        assert_eq!(906609, get_highest_palindrome_generated_by_two_number_of_n_digits(3).unwrap());
+        assert_eq!(906609, get_highest_palindrome_optimally_generated_by_two_number_of_3_digits().unwrap());
     }
 
     #[test]
     fn max_palindrome_made_out_of_n_digit_number() {
         assert_eq!((91, 99), get_numbers_to_generate_max_palindrome_with_n_digits(2).unwrap());
         assert_eq!((913, 993), get_numbers_to_generate_max_palindrome_with_n_digits(3).unwrap());
+    }
+
+    #[test]
+    fn prev_return_only_palindrome_numbers() {
+        let mut p =  Palindrome::new(999);
+        for i in (0..9).rev() {
+            p.mutate_to_prev();
+            assert_eq!((90 + i) * 10 + 9, p.p);
+        }
+        p.mutate_to_prev();
+        assert_eq!(898, p.p);
+    }
+
+    #[test]
+    fn corner_cases() {
+        let mut p =  Palindrome::new(1001);
+        p.mutate_to_prev();
+        assert_eq!(999, p.p);
+        let mut p =  Palindrome::new(101);
+        p.mutate_to_prev();
+        assert_eq!(99, p.p);
+        let mut p =  Palindrome::new(11);
+        p.mutate_to_prev();
+        assert_eq!(9, p.p);
     }
 
     #[test]
